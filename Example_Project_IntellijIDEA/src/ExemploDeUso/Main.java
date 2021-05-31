7b5855b44faf46169d71e775d7aa78f07826d753
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-05-31 13:16:20 -0300

    Novo exemplo de uso

--- Example_Project_IntellijIDEA/src/ExemploDeUso/Main.java
+++ Example_Project_IntellijIDEA/src/ExemploDeUso/Main.java
@@ -1,7 +1,34 @@
 package ExemploDeUso;
 
+import net.infonode.gui.laf.InfoNodeLookAndFeel;
+import net.infonode.gui.laf.InfoNodeLookAndFeelTheme;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        new Tela();
+        try {
+            InfoNodeLookAndFeelTheme theme = new InfoNodeLookAndFeelTheme("My Theme",
+                    new Color(166, 166, 166),
+                    new Color(255, 255, 255),
+                    Color.DARK_GRAY,
+                    Color.WHITE,
+                    new Color(252, 83, 28),
+                    Color.WHITE,
+                    0.8);
+
+            UIManager.setLookAndFeel(new InfoNodeLookAndFeel(theme));
+            new Tela("Example");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Ocorreu um problema, entre em contato com o desenvolvedor");
+
+        }
+
     }
 }
+
